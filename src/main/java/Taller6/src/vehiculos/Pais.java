9ef284a625d2d0ba9b48b459e9e3965f610884c5
jonatan8254 <josanchezv@unal.edu.co>
2023-03-28 13:11:16 -0500

    Update Pais.java

--- src/main/java/Taller6/src/vehiculos/Pais.java
+++ src/main/java/Taller6/src/vehiculos/Pais.java
@@ -1,5 +1,44 @@
 package vehiculos;
+import java.util.ArrayList; 
+
 
 public class Pais {
-
+	private String nombre;
+	private int cantventa;
+	protected static ArrayList listaPaises=new ArrayList();
+	
+	public Pais(String nombre) {
+		this.nombre=nombre;
+		
+		
+		
+		listaPaises.add(this);
+		listaPaises.add(0);
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+	
+	
+	
+	
+	public static Pais paisMasVendedor() {
+		int ventamayor=-1;
+		Pais paismayor=null;
+		for (int i = 1; i < listaPaises.size(); i += 2) { 
+			if ((int)listaPaises.get(i)>ventamayor){
+				ventamayor=(int)listaPaises.get(i);
+				paismayor=(Pais)listaPaises.get(i-1);	
+			}
+		}
+		
+		return paismayor;
+	} 
+		
 }
